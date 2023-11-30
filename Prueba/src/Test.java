50fa9e177a5776e3630e0231640b10c2c9181bd8
Usuario <juan.gomez.freelance@gmail.com>
2023-11-30 16:50:18 +0100

    prueba

---
+++ Prueba/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		int[] num=new int[2];
+        num[0]=16;
+	    num[1]=25;
+	    for (int i : num) {
+			System.out.println(i);
+		}
+	}
+
+}
